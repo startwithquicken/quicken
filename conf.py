e480e957914ea5e0dff3cd1750f88aced4b56366
startwithquicken <merryflee66+9@gmail.com>
2026-02-12 14:17:13 +0530

    Update conf.py

--- conf.py
+++ conf.py
@@ -1,4 +1,13 @@
 project = 'Quicken Help Guide'
-author = 'Support Documentation'
-extensions = ['myst_parser']
+author = 'Quicken Documentation Team'
+release = '1.0'
+
+extensions = [
+    'myst_parser'
+]
+
+templates_path = ['_templates']
+exclude_patterns = []
+
 html_theme = 'alabaster'
+html_static_path = ['_static']
